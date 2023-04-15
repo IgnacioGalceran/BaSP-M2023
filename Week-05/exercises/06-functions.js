aa13026d5174ac43b25128c8af828e648e5f9557
Ignacio <igna.galceran@gmail.com>
2023-04-15 07:31:27 -0300

    BaSP-W05-2023: ex06-functions

---
+++ Week-05/exercises/06-functions.js
@@ -0,0 +1,73 @@
+console.log('--EXERCISE 6: FUNCTIONS');
+
+/* a. Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el
+resultado en una variable, mostrando el valor de dicha variable en la consola del navegador. */
+
+console.log('-Exercise 6.a:');
+function add(num1, num2) {
+  return num1 + num2;
+}
+var addition = add(10, 4);
+console.log(addition);
+
+/* b. Copiar la función suma anterior y agregarle una validación para controlar si alguno de los parámetros
+no es un número; de no ser un número, mostrar un alert aclarando que uno de los
+parámetros tiene error y retornar el valor NaN como resultado. */
+
+console.log('-Exercise 6.b:');
+function addValid(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) {
+    alert('One or both params are not a number');
+    return NaN;
+  } else return Number(num1) + Number(num2);
+}
+
+/* c. Crear una función “validateInteger” que reciba un número como parámetro y
+devuelva verdadero si es un número entero. */
+
+console.log('-Exercise 6.c:');
+function validInteger(num) {
+  if (Number.isInteger(num)) return true;
+  else return false;
+}
+
+/* d. Copiar y renombrar la función suma del ejercicio 6b) y agregarle una llamada a la función del ejercicio 6c.
+y que valide que los números sean enteros. En caso que haya decimales mostrar un alert con el error y
+retornar el número convertido a entero (redondeado). */
+
+console.log('-Exercise 6.d:');
+function addIntValid(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) {
+    alert('One or both params are not a number');
+    return NaN;
+  } else {
+    if (validInteger(Number(num1)) && validInteger(Number(num2)))
+      return Number(num1) + Number(num2);
+    else {
+      alert('One or both params are not integers');
+      return Math.round(Number(num1)) + Math.round(Number(num2));
+    }
+  }
+}
+
+/* e. Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de una nueva función
+probando que todo siga funcionando igual que en el apartado anterior. */
+
+console.log('-Exercise 6.e:');
+function addIntValid2(num1, num2) {
+  if (isNaN(num1) || isNaN(num2)) {
+    alert('One or both params are not a number');
+    return NaN;
+  } else {
+    if (Number.isInteger(Number(num1)) && Number.isInteger(Number(num2)))
+      return Number(num1) + Number(num2);
+    else {
+      alert('One or both params are not integers');
+      return Math.round(Number(num1)) + Math.round(Number(num2));
+    }
+  }
+}
+
+function addWithValidation(num1, num2) {
+  return addIntValid2(num1, num2);
+}
